cf23c073adddb013c1a37bc463649b8a01b821e3
Courtney Cash <cashcourtney97@gmail.com>
2022-01-20 12:35:57 -0500

    adding serializers and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :address, presence: true
+    validates :email, presence: true, uniqueness:
+    true
 end
