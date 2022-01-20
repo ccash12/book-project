229f4e4799f507b8c7d9e9e60a28bfe0e6c7a3e1
Courtney Cash <cashcourtney97@gmail.com>
2022-01-20 12:42:21 -0500

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 puts "Start Seeding..."
 
 u1 = User.create(first_name: "Courtney", last_name: "Cash", address: "456 Street", email: "courtney@gmail.com", password: "password")
+
+b1 = Book.create(name:, author: , rating: , description: )
