dd7d8b1828854b6b9c51d5d21d0539983f636b11
Courtney Cash <cashcourtney97@gmail.com>
2022-01-20 12:41:10 -0500

    created migration

--- db/migrate/20220103164532_create_books.rb
+++ db/migrate/20220103164532_create_books.rb
@@ -1,7 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :address
+      t.string :email
+      t.string :password_digest
       t.timestamps
     end
   end
