a7211631a326c9dc6d32adc42b259bad663e0acc
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-07-28 12:45:53 -0700

    working on Navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,29 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 function NavBar() {
 
 
     return (
-        <div>
-
-        </div>
+        <div className="NavBarContainer">
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand to="/"href="home" className="NavBarTitle">Courtney Cash</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="navBarLinks">
+            <Nav.Link to="/Books" href="Books">Books</Nav.Link>
+            <Nav.Link to="/projects" href="projects">Projects</Nav.Link>
+            <Nav.Link to="/profile" href="profile">Profile</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
     );
 }
 
