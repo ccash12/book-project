50dfdc87bfbf93dd91ced21823e33617e1f770a4
Courtney Cash <84878740+ccash12@users.noreply.github.com>
2023-08-17 09:07:23 -0700

    adding new images

--- client/src/components/ImageData.js
+++ client/src/components/ImageData.js
@@ -1,12 +1,12 @@
 export const ImageData = [
     {
-        image:"https://www.acouplecooks.com/wp-content/uploads/2019/11/Recipes-Header-1-800x400.jpg"
+        image:""
     },
     {
-        image:"https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk="
+        image:""
     }, 
     {
-        image:"https://media.istockphoto.com/photos/dinner-table-full-of-food-picture-id1136956875?k=20&m=1136956875&s=612x612&w=0&h=RxWNKQSgpOrgdw0DeR_sH6KyPrxp9znVdBYzLn1Y7Hc="
+        image:""
     },
 
 ]
